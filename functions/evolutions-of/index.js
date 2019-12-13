ad015dd499d64f11e4002b06582af711c7d536f0
Maflooty <mmaflooty@gmail.com>
2019-12-13 20:57:33 +0100

    evolutions-of

--- functions/evolutions-of/index.js
+++ functions/evolutions-of/index.js
@@ -1,15 +1,26 @@
 const evolutionOf = (pokeArray, name) => {
-      const pokemon = pokeArray.find(p => p.name === name);
-      if(!pokemon) return [];
-      const allEvolutions = [];
-      if(Array.isArray(pokemon.prev_evolution)) {
-          allEvolutions.push(...pokemon.prev_evolution);
-      }
-      allEvolutions.push({num: pokemon.num, name: pokemon.name});
-      if(Array.isArray(pokemon.next_evolution)) {
-          allEvolutions.push(...pokemon.next_evolution);
-      }
-      return allEvolutions;
+     
+    const pokemon = pokeArray.find(p => p.name === name);
+      
+    if(!pokemon) return [];
+      
+    const allEvolutions = [];
+     
+    if(Array.isArray(pokemon.prev_evolution)) {
+        
+        allEvolutions.push(...pokemon.prev_evolution);
+     
+    }
+      
+    allEvolutions.push({num: pokemon.num, name: pokemon.name});
+     
+    if(Array.isArray(pokemon.next_evolution)) {
+        
+        allEvolutions.push(...pokemon.next_evolution);
+     
+    }
+      
+    return allEvolutions;
 };
 
 module.exports = evolutionOf;
