af9e98059efa2dec5b4f2bcf47384da29dbf6dbd
Maflooty <mmaflooty@gmail.com>
2019-12-13 09:54:38 +0100

    find-by/type

--- functions/find-by/type/index.js
+++ functions/find-by/type/index.js
@@ -1,5 +1,7 @@
 const type = (pokeArray, type) => {
-
+let results = pokeArray.filter(p => p.type.includes(type));
+results = results.map(p => ({ name: p.name, num: p.num }));
+return results;
 };
 
 module.exports = type;
