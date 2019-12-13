ab1e35fcfa70d3db0f1ed3980d77074f81d61421
Maflooty <mmaflooty@gmail.com>
2019-12-13 20:49:32 +0100

    find-by/key-value

--- functions/find-by/key-value/index.js
+++ functions/find-by/key-value/index.js
@@ -3,7 +3,22 @@
 // (later you will learn about req.body to pass any type)
 
 const keyValue = (pokeArray, key, value) => {
+   
+    const keyValueObject = pokeArray.find(element => element[key] == value);
+    
+    if(!keyValueObject) return [];
 
+    const resultArray = pokeArray.filter(element => element[key] == value);
+   
+    const resultArrayExpected = [];
+
+    resultArray.forEach(element => {
+        
+        resultArrayExpected.push({'num': element.num, 'name': element.name})
+        
+    });
+    
+    return resultArrayExpected;
 };
 
 
